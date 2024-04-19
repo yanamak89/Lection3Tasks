c80e18887b2e68ff93bec1e7b6b4c525fa7494d5
Yana Makogon <yana.mak89@gmail.com>
2024-04-19 12:03:55 +0200

    Task2 completed

--- HomeTasks/Program.cs
+++ HomeTasks/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, world!");
+    }
+}
